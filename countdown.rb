be10c34248f98bf6c45a3e8f44653750a17b00ff
Kristina <kristina.hodges7@gmail.com>
2022-08-29 09:40:05 -0400

    commit tree 535a7badd61ae19a772a6de6db3f6a70df26d845

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
+require 'pry'
 
-def countdown
+def countdown int 
+  number = int
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep int 
+  sleep(int)
 end
